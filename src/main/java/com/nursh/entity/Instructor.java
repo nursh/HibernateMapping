c03a98ab879393622cf1e08def6aa42da66339a2
nursh <nsheikhmohammed@yahoo.com>
2018-09-09 19:39:28 -0400

    Initial commit

---
+++ src/main/java/com/nursh/entity/Instructor.java
@@ -0,0 +1,35 @@
+package com.nursh.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "instructor")
+@Entity
+public class Instructor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
